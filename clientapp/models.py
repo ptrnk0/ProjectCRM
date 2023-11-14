0c56cddb7f5fdc88e119ed16a1a931d3f9e09376
Tarlan <mikailtechcorp@gmail.com>
2023-11-14 14:49:24 +0200

    added select to staff sex field in model and made some tunning of site appearance in templates

--- clientapp/models.py
+++ clientapp/models.py
@@ -8,7 +8,7 @@ def user_directory_path(instance, filename):
 class Client(models.Model):
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=17)
     email = models.EmailField(max_length=30, blank=True, default='')
     birthday = models.DateField(null=True, blank=True, default=None)
     comment = models.CharField(max_length=150, blank=True, default='')
