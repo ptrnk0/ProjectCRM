eef82348ba32c7e6712869df84cc655eb1e88a16
ptrnk0 <140536679+ptrnk0@users.noreply.github.com>
2023-11-19 09:35:44 +0200

    Update views.py

--- serviceapp/views.py
+++ serviceapp/views.py
@@ -71,7 +71,6 @@ class ListResourceForServiceView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
         context["resource_list"] = Resource.objects.all()
         return context
 
