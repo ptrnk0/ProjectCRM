4d0831896c3bfed2fa43d41d57c55c96e83b2a84
ptrnko <vlad.ptrnko@gmail.com>
2023-11-26 22:13:19 +0200

    Refactor staff.

--- staffapp/models.py
+++ staffapp/models.py
@@ -1,24 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
 
 
-class Staff(models.Model):
-    gender = [('man', 'man'), ('woman', 'woman')]
-
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=15)
-    sex = models.CharField(max_length=15, choices=gender)
-    email = models.CharField(max_length=30, blank=True, default='')
-    phone = models.CharField(max_length=16)
-    job_title = models.CharField(max_length=15)
-    access_level = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+# class Staff(models.Model):
+#     gender = [('man', 'man'), ('woman', 'woman')]
+#
+#     first_name = models.CharField(max_length=15)
+#     last_name = models.CharField(max_length=15)
+#     sex = models.CharField(max_length=15, choices=gender)
+#     email = models.CharField(max_length=30, blank=True, default='')
+#     phone = models.CharField(max_length=16)
+#     job_title = models.CharField(max_length=15)
+#     access_level = models.IntegerField()
+#
+#     def __str__(self):
+#         return f'{self.first_name} {self.last_name}'
 
 
 class Schedule(models.Model):
     date = models.DateField()
-    id_staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    id_staff = models.ForeignKey(User, on_delete=models.CASCADE)
     start_time = models.TimeField()
     end_time = models.TimeField()
