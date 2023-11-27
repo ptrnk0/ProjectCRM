44db4cdea3039113e8065fe7e3ebbc7bdcb280e7
Tarlan <mikailtechcorp@gmail.com>
2023-11-27 12:04:12 +0200

    added total price calculation to model order

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
-from staffapp import models as staff
+from decimal import Decimal
 
 
 class Commodity(models.Model):
     name = models.CharField(max_length=50)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     amount = models.IntegerField()
 
     class Meta:
@@ -20,10 +20,17 @@ class Order(models.Model):
     id_commodity = models.ForeignKey(Commodity, on_delete=models.PROTECT, related_name='commodity')
     id_employee = models.ForeignKey(User, on_delete=models.PROTECT, related_name='staff')
     amount = models.IntegerField()
-    discount = models.FloatField(null=True, default=None)
+    discount = models.IntegerField(null=True, default=None)
 
     class Meta:
         db_table = 'Order'
 
     def __str__(self):
         return f'{self.date}, {self.id_commodity}, {self.id_employee}, {self.amount}, {self.discount}'
+
+    @property
+    def total_price(self):
+        if self.discount == None:
+            return self.id_commodity.price
+        else:
+            return f'{self.id_commodity.price * Decimal(1 - self.discount/100):.2f}'
